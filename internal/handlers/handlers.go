86d23519d56cfb91c684df1be034893fc1b39a75
Sergey Mikryukov <mikeyuniversezero@gmail.com>
2021-11-17 22:39:29 +0300

    Write returned types

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,16 +1,25 @@
 package handlers
 
 import (
+	"backend-task/internal/models"
 	"backend-task/internal/repository"
 	"backend-task/pgk/logger"
 )
 
-type Handlers struct{}
-
-func New(repo *repository.Repository, workersNum int, logger *logger.Logger) *Handlers {
-	return &Handlers{}
+type Handlers struct {
+	Repo *repository.Repository
 }
 
-func (h *Handlers) AddTask() {}
+func New(repo *repository.Repository, workersNum int, logger *logger.Logger) *Handlers {
+	return &Handlers{Repo: repo}
+}
 
-func (h *Handlers) Task() {}
+func (h *Handlers) AddTask(task models.TaskAddInput) error {
+	// Вызвать метод AddTask у репозитория
+	return nil
+}
+
+func (h *Handlers) Task() ([]models.TaskResultOutput, error) {
+	// Вызвать метод AddTask у репозитория
+	return []models.TaskResultOutput{}, nil
+}

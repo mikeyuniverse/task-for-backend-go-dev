666005f96b90835ec85518ff362796ba1d1f3af3
Sergey Mikryukov <mikeyuniversezero@gmail.com>
2021-11-17 22:39:47 +0300

    Change unimported field to imported

--- internal/models/task.go
+++ internal/models/task.go
@@ -3,9 +3,9 @@ package models
 import "time"
 
 type TaskAddInput struct {
-	n   int     // Amount of element
-	d   float32 // Delta between elements
-	n1  float32 // Start value
+	N   int     // Amount of element
+	D   float32 // Delta between elements
+	N1  float32 // Start value
 	I   int     // interval in seconds
 	TTL int     // time to life for result
 }
@@ -13,9 +13,9 @@ type TaskAddInput struct {
 type TaskResultOutput struct {
 	QueueNum       int       // number in the queue
 	Status         string    // inQueue | inProgress | Done
-	n              int       // Amount of element
-	d              float32   // Delta between elements
-	n1             float32   // Start value
+	N              int       // Amount of element
+	D              float32   // Delta between elements
+	N1             float32   // Start value
 	I              int       // interval in seconds
 	TTL            int       // time to life for result
 	nowIter        int       // current iteration

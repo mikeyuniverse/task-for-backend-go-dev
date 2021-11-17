7e863eba7e8f7844dd65b96db6db3892728057d7
Sergey Mikryukov <mikeyuniversezero@gmail.com>
2021-11-17 23:20:16 +0300

    Write repository handler

--- internal/repository/repo.go
+++ internal/repository/repo.go
@@ -1,13 +1,26 @@
 package repository
 
 import (
+	"backend-task/internal/models"
+	"backend-task/internal/repository/queue"
 	"backend-task/pgk/logger"
 )
 
 type Repository struct {
-	ttl int64
+	ttl   int64
+	queue *queue.Queue
 }
 
 func New(ttl int64, logger *logger.Logger) *Repository {
-	return &Repository{ttl: ttl}
+	return &Repository{ttl: ttl, queue: queue.NewQueue(ttl)}
+}
+
+func (r *Repository) AddTaskToQueue(task models.TaskResultOutput) error {
+	// Добавляет задачу в очередь
+	return r.queue.AddTask(task)
+}
+
+func (r *Repository) GetAllCurrentTasks() []models.TaskResultOutput {
+	// Получает всю текущую очередь, в отсортированном порядке по времени поступления задачи на обработку
+	return r.queue.GetAllTasks()
 }

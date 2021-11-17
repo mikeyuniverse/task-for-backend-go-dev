a9d9d8a61eb136a24fd57b1671b975bf83876e62
Sergey Mikryukov <mikeyuniversezero@gmail.com>
2021-11-17 23:50:14 +0300

    Change type in TaskResultOutput: TTL: int->int64 TaskTime: time.Time->int64

--- internal/models/task.go
+++ internal/models/task.go
@@ -1,7 +1,5 @@
 package models
 
-import "time"
-
 type TaskAddInput struct {
 	N   int     // Amount of element
 	D   float32 // Delta between elements
@@ -11,17 +9,17 @@ type TaskAddInput struct {
 }
 
 type TaskResultOutput struct {
-	QueueNum       int       // number in the queue
-	Status         string    // inQueue | inProgress | Done
-	N              int       // Amount of element
-	D              float32   // Delta between elements
-	N1             float32   // Start value
-	I              int       // interval in seconds
-	TTL            int       // time to life for result
-	NowIter        int       // current iteration
-	CreateTaskTime time.Time // time where task added
-	StartTaskTime  time.Time // time where task started
-	DoneTaskTime   time.Time // time where task completed
+	QueueNum       int     // number in the queue
+	Status         string  // inQueue | inProgress | Done
+	N              int     // Amount of element
+	D              float32 // Delta between elements
+	N1             float32 // Start value
+	I              int     // interval in seconds
+	TTL            int64   // time to life for result
+	NowIter        int     // current iteration
+	CreateTaskTime int64   // time where task added
+	StartTaskTime  int64   // time where task started
+	DoneTaskTime   int64   // time where task completed
 }
 
 type TaskCalculateProgressionInput struct {

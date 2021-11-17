369c173019f629d5f133403bb87e2c1cbb9e7d07
Sergey Mikryukov <mikeyuniversezero@gmail.com>
2021-11-17 22:05:40 +0300

    cleaned up

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -5,26 +5,12 @@ import (
 	"backend-task/pgk/logger"
 )
 
-// import (
-// 	"backend-task/internal/models"
-// 	"backend-task/internal/repository"
-// 	"backend-task/pgk/logger"
-// )
+type Handlers struct{}
 
-type Handlers struct {
-	queue *Queue
+func New(repo *repository.Repository, workersNum int, logger *logger.Logger) *Handlers {
+	return &Handlers{}
 }
 
-func New(repo *repository.Repository, ttl int64, workersNum int, logger *logger.Logger) *Handlers {
-	// 	// Здесь надо запустить воркеры, создать каналы
-	// 	forStart := make(chan models.TaskCalculateProgressionInput, workersNum)
+func (h *Handlers) AddTask() {}
 
-	// 	for i := 0; i < workersNum; i++ {
-	// 		go countArithmeticProgression(forStart)
-	// 	}
-	return &Handlers{queue: newQueue(ttl)}
-}
-
-// func (h *Handlers) AddTask() {}
-
-// func (h *Handlers) Task() {}
+func (h *Handlers) Task() {}

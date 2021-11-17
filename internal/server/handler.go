5d03c95761bb2fa5d503037859ea515a318a5617
Sergey Mikryukov <mikeyuniversezero@gmail.com>
2021-11-17 21:59:32 +0300

    Del repository from requests handler

--- internal/server/handler.go
+++ internal/server/handler.go
@@ -2,18 +2,16 @@ package server
 
 import (
 	"backend-task/internal/handlers"
-	"backend-task/internal/repository"
 
 	"github.com/gin-gonic/gin"
 )
 
 type serverHandler struct {
-	repo    *repository.Repository
 	handler *handlers.Handlers
 }
 
-func initHandlers(repo *repository.Repository, handlers *handlers.Handlers) *serverHandler {
-	return &serverHandler{repo: repo, handler: handlers}
+func initHandlers(handlers *handlers.Handlers) *serverHandler {
+	return &serverHandler{handler: handlers}
 }
 
 func (h *serverHandler) AddTask(c *gin.Context) {

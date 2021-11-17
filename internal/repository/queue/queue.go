eb4ba48d53d5fa6609daa4f76ad64f67b53f8fce
Sergey Mikryukov <mikeyuniversezero@gmail.com>
2021-11-17 23:27:32 +0300

    Add mutex. Del TTL.

--- internal/repository/queue/queue.go
+++ internal/repository/queue/queue.go
@@ -1,24 +1,30 @@
 package queue
 
-import "backend-task/internal/models"
+import (
+	"backend-task/internal/models"
+	"sync"
+)
 
 // В этом пакете должна происходить цикличная проверка TTL элементов очереди
 
 type Queue struct {
-	elems               []models.TaskResultOutput
-	ttl                 int64
-	CurrentQueueCounter int
+	elems []models.TaskResultOutput
+	mutex *sync.Mutex
 }
 
-func NewQueue(ttl int64) *Queue {
-	return &Queue{elems: make([]models.TaskResultOutput, 0), ttl: ttl, CurrentQueueCounter: 0}
+func NewQueue() *Queue {
+	return &Queue{elems: make([]models.TaskResultOutput, 0)}
 }
 
 func (q *Queue) AddTask(task models.TaskResultOutput) error {
+	q.mutex.Lock()
 	q.elems = append(q.elems, task)
+	q.mutex.Unlock()
 	return nil
 }
 
 func (q *Queue) GetAllTasks() []models.TaskResultOutput {
+	q.mutex.Lock()
+	defer q.mutex.Unlock()
 	return q.elems
 }

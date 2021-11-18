0dac33e393d8090fa2220f80cd6b1518fcc10741
Sergey Mikryukov <mikeyuniversezero@gmail.com>
2021-11-18 21:12:35 +0300

    Change Handlers struct and constructor. Create increment iteration num func.

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -8,14 +8,31 @@ import (
 )
 
 type Handlers struct {
-	Repo *repository.Repository
+	Repo       *repository.Repository
+	workersNum int
+	logger     *logger.Logger
+	chNewTask  chan models.TaskResultOutput // Channel for receive new task
+	chDoneTask chan models.TaskResultOutput // Channel for receive completed task
 }
 
 func New(repo *repository.Repository, workersNum int, logger *logger.Logger) *Handlers {
-	return &Handlers{Repo: repo}
+	h := &Handlers{
+		Repo:       repo,
+		workersNum: workersNum,
+		logger:     logger,
+		chNewTask:  make(chan models.TaskResultOutput, workersNum),
+		chDoneTask: make(chan models.TaskResultOutput, workersNum),
+	}
+
+	h.workersInit(workersNum) // Create workers
+	go h.doneTaskProcessing() // Processing completed tasks
+	go h.addJobForWorkers()   // Adding tasks to work for workers
+
+	return h
 }
 
 func (h *Handlers) AddTask(task models.TaskAddInput) error {
+	h.logger.Info("AddTask")
 	newTask := models.TaskResultOutput{
 		Status:         "inQueue",
 		N:              task.N,
@@ -33,4 +50,10 @@ func (h *Handlers) Task() []models.TaskResultOutput {
 	return h.Repo.GetAllCurrentTasks()
 }
 
-// В этом пакете должна реализовываться логика расчета арифметической прогрессии
+func (h *Handlers) incrementIterationNum(task models.TaskResultOutput) {
+	h.Repo.IncrementIterationNum(task)
+}
+
+func (h *Handlers) doneTask(task models.TaskResultOutput) {
+	h.Repo.DoneTask(task)
+}

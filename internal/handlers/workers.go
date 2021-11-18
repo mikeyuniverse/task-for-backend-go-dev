c404e125ef291175c3480833a24d35f6817c5ce1
Sergey Mikryukov <mikeyuniversezero@gmail.com>
2021-11-18 22:48:17 +0300

    Add ticker in addJobForWorkers. Change channels: value -> pointer.

--- internal/handlers/workers.go
+++ internal/handlers/workers.go
@@ -1,5 +1,9 @@
 package handlers
 
+import (
+	"time"
+)
+
 func (h *Handlers) workersInit(workersCount int) {
 	for i := 0; i < workersCount; i++ {
 		go h.calcProgression()
@@ -7,19 +11,20 @@ func (h *Handlers) workersInit(workersCount int) {
 }
 
 func (h *Handlers) addJobForWorkers() {
-	for {
+	ticker := time.NewTicker(time.Millisecond * 500)
+	for range ticker.C {
 		task, exist := h.Repo.GetTaskNotInWork()
 		if !exist {
 			// If tasks not exists - nothing
 			continue
 		}
 		h.Repo.ChangeTaskStatus(task, "inProgress")
-		h.chNewTask <- task
+		*h.chNewTask <- task
 	}
 }
 
 func (h *Handlers) doneTaskProcessing() {
-	for task := range h.chDoneTask {
+	for task := range *h.chDoneTask {
 		h.doneTask(task)
 	}
 }

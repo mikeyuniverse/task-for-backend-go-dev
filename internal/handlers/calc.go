c4f11876311839a7229e1d2507753dc3c8312ee4
Sergey Mikryukov <mikeyuniversezero@gmail.com>
2021-11-18 22:47:15 +0300

    Change value of channels to pointer of channels

--- internal/handlers/calc.go
+++ internal/handlers/calc.go
@@ -5,14 +5,15 @@ import (
 )
 
 func (h *Handlers) calcProgression() {
-	for task := range h.chNewTask {
+	for task := range *h.chNewTask {
 		progresion := make([]float32, task.N)
 		progresion[0] = task.N1
+
 		for i := 1; i < task.N; i++ {
 			progresion[i] = progresion[i-1] + task.D
 			h.incrementIterationNum(task)
 			time.Sleep(time.Duration(task.I) * time.Second)
 		}
-		h.chDoneTask <- task
+		*h.chDoneTask <- task
 	}
 }

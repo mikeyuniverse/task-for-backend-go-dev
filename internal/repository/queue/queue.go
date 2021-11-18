4d634660983627e5216ecdbbdd1d8420ec4e6934
Sergey Mikryukov <mikeyuniversezero@gmail.com>
2021-11-18 21:14:08 +0300

    Added a bunch of methods

--- internal/repository/queue/queue.go
+++ internal/repository/queue/queue.go
@@ -6,17 +6,12 @@ import (
 	"time"
 )
 
-// В этом пакете должна происходить цикличная проверка TTL элементов очереди
-// TODO Сделать метод получения первого элемента из очереди
-// TODO Сделать метод обновления номеров в очереди
-// TODO Сделать метод обновления данных в задаче в очереди
-
 type Queue struct {
 	elems []models.TaskResultOutput
 	mutex *sync.Mutex
 }
 
-func NewQueue() *Queue {
+func New() *Queue {
 	queue := &Queue{elems: make([]models.TaskResultOutput, 0)}
 	go queue.runTTLchecker()
 	return queue
@@ -25,14 +20,46 @@ func NewQueue() *Queue {
 func (q *Queue) AddTask(task models.TaskResultOutput) error {
 	q.mutex.Lock()
 	q.elems = append(q.elems, task)
+	q.recalcQueueNum()
 	q.mutex.Unlock()
 	return nil
+}
+
+func (q *Queue) GetTaskNotInWork() (models.TaskResultOutput, bool) {
+	// Receive first task in queue
+	// Returned task and bool (true - task exist)
+	for _, task := range q.elems {
+		if task.Status == "inQueue" {
+			return task, true
+		}
+	}
+	return models.TaskResultOutput{}, false
 }
 
 func (q *Queue) GetAllTasks() []models.TaskResultOutput {
 	q.mutex.Lock()
 	defer q.mutex.Unlock()
 	return q.elems
+}
+
+func (q *Queue) ChangeTaskStatus(task models.TaskResultOutput, newStatus string) {
+	q.mutex.Lock()
+	defer q.mutex.Unlock()
+	q.elems[task.QueueNum-1].Status = newStatus
+	q.elems[task.QueueNum-1].StartTaskTime = time.Now().Unix()
+}
+
+func (q *Queue) DoneTask(task models.TaskResultOutput) {
+	q.mutex.Lock()
+	defer q.mutex.Unlock()
+	q.elems[task.QueueNum-1].DoneTaskTime = time.Now().Unix()
+	q.elems[task.QueueNum-1].Status = "Done"
+}
+
+func (q *Queue) IncIter(task models.TaskResultOutput) {
+	q.mutex.Lock()
+	defer q.mutex.Unlock()
+	q.elems[task.QueueNum-1].NowIter = q.elems[task.QueueNum-1].NowIter + 1
 }
 
 func (q *Queue) runTTLchecker() {
@@ -45,7 +72,9 @@ func (q *Queue) runTTLchecker() {
 func (q *Queue) checkTTL() {
 	for index, elem := range q.elems {
 		if elem.TTL >= time.Now().Unix() {
-			q.deleteElementFromQueue(index)
+			if elem.Status != "inProgress" {
+				q.deleteElementFromQueue(index)
+			}
 		}
 	}
 }
@@ -56,5 +85,18 @@ func (q *Queue) deleteElementFromQueue(index int) {
 	rightSide := q.elems[index+1:]
 	mergedList := append(leftSide, rightSide...)
 	q.elems = mergedList
+	q.recalcQueueNum()
 	q.mutex.Unlock()
+}
+
+func (q *Queue) recalcQueueNum() {
+	// Recalculates the item numbers in the queue
+	q.mutex.Lock()
+	defer q.mutex.Unlock()
+	for index, item := range q.elems {
+		if item.Status == "inProgress" {
+			continue
+		}
+		item.QueueNum = index + 1
+	}
 }
